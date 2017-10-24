8e21c9169e894368eddb9a8ff8183c7feaca54dc
Tiago Schoeping Reinert <tiago.schoeping@gmail.com>
2017-10-24 10:28:10 -0200

    Script added

---
+++ runScript.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ "$#" -ne 3 ]; then
+    echo "3 parametros: <arquivo> <quantidade de itens> <quantidade de execuções>"
+    return
+fi
+
+mkdir -p resultados
+
+execucoes=$3
+tamanho=$2
+script=$1
+
+for p in 1 2 4 8 10
+do
+	sum=0
+	echo Inicando execução do script "$script" com "$execucoes" execuções, utilizando "$p" threads
+	for i in $(seq 1 $execucoes)
+	do
+		output=$(./"$script" "$tamanho" "$p")
+		sum=$(($sum + $output))
+		#echo "$p" $output >> resultados/"$script".txt
+	done
+	echo sum: $sum
+done
+
+#gnuplot -e "set xrange [0:10]; set term png; set output 'print.png'; stats \$data using ;plot 'resultados/$script.txt'"
